f616545a64fe5ef179c81d53d842f9034aca9adc
Stefaan <safwebdev@gmail.com>
2019-10-29 12:40:30 +0800

    Build chat room

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,80 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<div class="header">
+			<h1>Chatroom</h1>
+			<p class="username">Username: {{ username }}</p>
+			<p class="online">Online: {{ users.length }}</p>
+		</div>
+		<ChatRoom v-bind:messages="messages" v-on:sendMessage="this.sendMessage" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import io from 'socket.io-client'
+	import ChatRoom from './components/ChatRoom'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	export default {
+		name: 'app',
+		components: {
+			ChatRoom
+		},
+		data: function () {
+			return {
+				username: "",
+				socket: io("http://localhost:3000"),
+				messages: [],
+				users: []
+			}
+		},
+		methods: {
+			joinServer: function () {
+				this.socket.on('loggedIn', data => {
+					this.messages = data.messages;
+					this.users = data.users;
+					this.socket.emit('newuser', this.username);
+				});
+				this.listen();
+			},
+			listen: function () {
+				this.socket.on('userOnline', user => {
+					this.users.push(user);
+				});
+				this.socket.on('userLeft', user => {
+					this.users.splice(this.users.indexOf(user), 1);
+				});
+				this.socket.on('msg', message => {
+					this.messages.push(message);
+				});
+			},
+			sendMessage: function (message) {
+				this.socket.emit('msg', message);
+			}
+		},
+		mounted: function () {
+			this.username = prompt("What is your username?", "Anonymous");
+			if (!this.username) {
+				this.username = "Anonymous";
+			}
+			this.joinServer();
+		}
+	}
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+	body {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		color: #2C3E50;
+		margin: 0;
+		padding: 0;
+	}
+	#app {
+		display: flex;
+		flex-direction: column;
+		height: 100vh;
+		width: 100%;
+		max-width: 768px;
+		margin: 0 auto;
+		padding: 15px;
+		box-sizing: border-box;
+	}
 </style>
--- server.js
+++ server.js
@@ -7,17 +7,16 @@ let index = 0;
 
 io.on("connection", socket => {
     socket.emit("loggedIn", {
-        users: users.map(s => username),
-        messages: messages
+		users: users.map(s => s.username),
+		messages: messages
     });
-
-    socket.on("newuser", socket => {
-        console.log(`${username} has arrived at the party.`);
-        socket.username = username;
-        user.push(socket);
-
-        io.emit("userOnline", socket.username);
-    });
+    
+    socket.on('newuser', username => {
+		console.log(`${username} has arrived at the party.`);
+		socket.username = username;	
+		users.push(socket);
+		io.emit('userOnline', socket.username);
+	});
 
     socket.on("msg", msg => {
         let message = {
@@ -26,20 +25,17 @@ io.on("connection", socket => {
             msg: msg 
         }
         messages.push(message);
-
         io.emit("msg", message);
-
         index++;
     });
 
-    // disconnect
+    // Disconnect
     socket.on("disconnect", () => {
         console.log(`${socket.username} has left the party.`);
         io.emit("userLeft", socket.username);
         users.splice(users.indexOf(socket), 1);
     });
 })
-
 
 http.listen(process.env.PORT || 3000 , () => {
     console.log("Listening on port %s", process.env.PORT || 3000);
